cde1199c425b5e56699a8d12db193a171e0f659d
twój login do GitHuba <s.k.kazmierczak7@gmail.com>
2022-10-06 01:37:38 +0200

    hide django secret_key

--- scrumlab/settings.py
+++ scrumlab/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rtm$@rcneyyjbo$u+)@z1dxy!qrtxc676h&zh97s+46)=2)@9*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
